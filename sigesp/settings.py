a941c1420e70d914ff9f9db14bb936273bd02bd9
repopna <repo.pna@gmail.com>
2025-01-14 00:33:25 +0100

    Allowed Hosts

--- sigesp/settings.py
+++ sigesp/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'sigesp2.kirene.ao', 'www.sigesp2.kirene.ao']
+ALLOWED_HOSTS = ['localhost', '0.0.0.0']
 
 
 # Application definition
